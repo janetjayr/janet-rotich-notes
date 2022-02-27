0b07d3f48874bf2f2289a591f6ab07c683c0e5b9
janetjayr <98049577+janetjayr@users.noreply.github.com>
2022-02-27 14:35:40 +0300

    Title:- python assignment 1 and 2 output
    the assignment is from lecture 7

---
+++ assignment1&2 output.py
@@ -0,0 +1,81 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+ This program computes the roots
+of a second equation:
+
+      ax^2 +bx +c =0     
+Please enter a: 5
+Please enter b: 10
+Please enter c: 2
+
+The roots are : 
+x1 = -2.254033307585166
+x2 =  -17.745966692414832
+>>> ================================ RESTART ================================
+>>> 
+ This program computes the roots
+of a second equation:
+
+      ax^2 +bx +c =0     
+Please enter a: 4
+Please enter b: 8
+Please enter c: 2
+
+The roots are : 
+x1 = -2.3431457505076194
+x2 =  -13.65685424949238
+>>> ================================ RESTART ================================
+>>> 
+ This program computes the roots
+of a second equation:
+
+      ax^2 +bx +c =0     
+Please enter a: 1
+Please enter b: 7
+Please enter c: 6
+
+The roots are : 
+x1 = -2.0
+x2 =  -12.0
+>>> ================================ RESTART ================================
+>>> 
+ This program computes the roots
+of a second equation:
+
+      ax^2 +bx +c =0     
+Please enter a: 1
+Please enter b: -2
+Please enter c: -15
+
+The roots are : 
+x1 = 10.0
+x2 =  -6.0
+>>> introduction_message()
+ This program computes the roots
+of a second equation:
+
+      ax^2 +bx +c =0     
+>>> input_and_solve()
+Please enter a: 7
+Please enter b: 14
+Please enter c: 2
+
+The roots are : 
+x1 = -2.1678404338007677
+x2 =  -25.83215956619923
+>>> final_message()
+
+Thank you for using the program
+===============================
+***   All rights reserved  ***
+
+>>> ================================ RESTART ================================
+>>> 
+hello world
+hello Again
+Yay!Printing.
+I'd much rather you'not'.
+I"said"do not touch this.
+>>> 
